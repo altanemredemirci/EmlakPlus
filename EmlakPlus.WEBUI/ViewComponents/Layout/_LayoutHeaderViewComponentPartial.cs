37672c9ce2fe71cb859c4a510620ebe37e2fd84d
Altan Emre <altan_emre_1989@hotmail.com>
2024-05-08 13:48:34 +0300

    index.html den _Layout oluşturuldu. Layout RenderBody() yaz Index.cshtml düzenle ViewComponents ler yazıldı *_LayoutHeaderViewComponentPartial.cs *_LayoutNavbarViewComponentPartial.cs *__LayoutFooterViewComponentPartial.cs *__LayoutScriptViewComponentPartial.cs

---
+++ EmlakPlus.WEBUI/ViewComponents/Layout/_LayoutHeaderViewComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmlakPlus.WEBUI.ViewComponents.Layout
+{
+    public class _LayoutHeaderViewComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ EmlakPlus.WEBUI/ViewComponents/Layout/_LayoutNavbarViewComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmlakPlus.WEBUI.ViewComponents.Layout
+{
+    public class _LayoutNavbarViewComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
