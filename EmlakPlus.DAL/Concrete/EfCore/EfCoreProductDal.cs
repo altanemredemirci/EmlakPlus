f71b8e8cdd1ecc6ec963ef81a661a2b05b1deb91
Altan Emre <altan_emre_1989@hotmail.com>
2024-05-13 14:35:30 +0300

    EfCoreProductDal\GetAll revize _PartialProduct.cshtml

--- EmlakPlus.DAL/Concrete/EfCore/EfCoreProductDal.cs
+++ EmlakPlus.DAL/Concrete/EfCore/EfCoreProductDal.cs
@@ -12,6 +12,15 @@ namespace EmlakPlus.DAL.Concrete.EfCore
 {
     public class EfCoreProductDal : EfCoreGenericRepository<Product, DataContext>, IProductDal
     {
+        public override List<Product> GetAll(Expression<Func<Product, bool>> filter)
+        {
+            using (var context = new DataContext())
+            {
+                return context.Products.Where(i => i.Status == true).Include(i => i.ProductType).Include(i => i.City).ToList();
+            }
+        }
+
+
         public List<Product> GetPopularAll()
         {
             using (var context = new DataContext())
