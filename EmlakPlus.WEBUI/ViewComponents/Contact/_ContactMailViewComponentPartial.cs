5abf0e67c9962cdb4cdaf7648cec1e1371640881
Altan Emre <altan_emre_1989@hotmail.com>
2024-05-13 17:52:52 +0300

    _ContactMailViewComponentPartial

---
+++ EmlakPlus.WEBUI/ViewComponents/Contact/_ContactMailViewComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmlakPlus.WEBUI.ViewComponents.Contact
+{
+    public class _ContactMailViewComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
