1baa2d475858a07cb209ea7d60a1f69ca4d0e609
Altan Emre <altan_emre_1989@hotmail.com>
2024-05-21 14:18:50 +0300

    _DashboardStatisticViewComponentPartial.cs

--- EmlakPlus.WEBUI/Program.cs
+++ EmlakPlus.WEBUI/Program.cs
@@ -61,6 +61,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Admin}/{action=Index}/{id?}");
 
 app.Run();
---
+++ EmlakPlus.WEBUI/ViewComponents/Dashboard/_DashboardStatisticViewComponentPartial.cs
@@ -0,0 +1,24 @@
+﻿using EmlakPlus.BLL.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmlakPlus.WEBUI.ViewComponents.Dashboard
+{
+    public class _DashboardStatisticViewComponentPartial:ViewComponent
+    {
+        private readonly IStatisticService _statisticService;
+
+        public _DashboardStatisticViewComponentPartial(IStatisticService statisticService)
+        {
+            _statisticService = statisticService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.TopAgencyByProductCount = _statisticService.TopAgencyByProductCount();
+            ViewBag.LastProductPrice = _statisticService.LastProductPrice();
+            ViewBag.TopCityByProductCount = _statisticService.TopCityByProductCount();
+            ViewBag.TopProductTypeByProductCount = _statisticService.TopProductTypeByProductCount();
+            return View();
+        }
+    }
+}
